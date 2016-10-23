898513d0525f684faefba4edde4b2befa0d5e74d
TylerConnors <tconnors14@mail.wou.edu>
2016-10-23 16:52:40 -0700

    Part of HW3

---
+++ cs460 HW3/Calculator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs460_HW3
+{
+	class Calculator
+	{
+		static void Main(string[] args)
+		{
+		}
+
+
+
+
+
+
+
+
+        public double doOperation(double a, double b, String s) 
+		{
+		double c = 0.0;
+			if (s.Equals("+"))      // Can't use a switch-case with Strings, so we do if-else
+				c = (a + b);
+			else if (s.Equals("-"))
+				c = (a - b);
+			else if (s.Equals("*"))
+				c = (a * b);
+			else if (s.Equals("/"))
+			{
+
+				try
+				{
+					c = (a / b);
+
+				}
+				catch (DivideByZeroException)
+				{
+					Console.WriteLine("Division of zero.");
+				}
+			}
+			else
+				throw new ArgumentException("Improper operator: " + s + ", is not one of +, -, *, or /");
+
+		return c;
+	}
+	}
+}
